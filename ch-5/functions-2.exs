1d94319e2bdd23fd24ca67458fa2fe2e3f0950fd
Andrea Zucchini <azga84@gmail.com>
2017-03-05 18:15:03 +0100

    functions chains

--- ch-5/functions-2.exs
+++ ch-5/functions-2.exs
@@ -17,3 +17,20 @@ IO.puts Funct.fizz_buzz(0, 0, "yeahhh!!") # FizzBuzz
 IO.puts Funct.fizz_buzz(0, 1, "yeahhh!!") # Fizz
 IO.puts Funct.fizz_buzz(1, 0, "yeahhh!!") # Buzz
 IO.puts Funct.fizz_buzz(1, 1, "yeahhh!!") # yeahhh!!
+IO.puts "******************************************"
+
+# The operator rem(a, b) returns the remainder after dividing a by b.
+# Write a function that takes a single integer (n) and calls the function in
+# the previ-ous exercise, passing it rem(n,3), rem(n,5), and n.
+# Call it seven times with the arguments 10, 11, 12, and so on. You should get
+# “Buzz, 11, Fizz, 13,14, FizzBuzz, 16.
+
+
+defmodule FunctTwo do
+  def call_function_fizz_buzz(number) do
+    Funct.fizz_buzz(rem(number, 3), rem(number,5), number)
+  end
+end
+
+range = 10..16
+IO.inspect  Enum.map(range, &(FunctTwo.call_function_fizz_buzz(&1))) # ["Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16]
