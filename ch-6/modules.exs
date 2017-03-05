99b9b2e0cf399b801ec52ad76e404c34a4296602
Andrea Zucchini <azga84@gmail.com>
2017-03-05 21:33:54 +0100

    length of list, sum numbers, greatest common divisor

--- ch-6/modules.exs
+++ ch-6/modules.exs
@@ -7,3 +7,31 @@ defmodule Times do
   def triple(number), do: number * 3
   def quadruple(number), do: double(number) * 2
 end
+
+
+defmodule Factorial do
+  def of(0), do: 1
+  def of(n), do: n * of(n - 1)
+end
+
+
+defmodule SumOfTheFirstXNumbers do
+  def sum(0), do: 0
+  def sum(n), do: n + sum(n - 1)
+end
+
+defmodule LengthOfList do
+  def get_length([]), do: 0
+  def get_length([head | tail]) do
+    1 + length(tail)
+  end
+end
+
+# Write a function gcd(x,y) that finds the greatest common divisor between
+# two nonnegative integers. Algebraically, gcd(x,y) is x if y is zero; it’s
+# gcd(y,rem(x,y)) otherwise.
+
+defmodule Maths do
+  def gcd(x, y) when y == 0, do: x
+  def gcd(x, y), do: gcd(y, rem(x, y))
+end
