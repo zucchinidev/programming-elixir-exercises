99b93d468f2ec2d84a3e19ade9e9d297cad682a6
Andrea Zucchini <azga84@gmail.com>
2017-03-06 01:56:06 +0100

    delete file folder

--- file/read-file.exs
+++
@@ -1,7 +0,0 @@
-handle_open = fn
-    { :ok, file } -> IO.puts "Show data: #{IO.read(file, :all)}"
-    { _, error } -> IO.puts "Error, file not found: #{:file.format_error(error)}"
-end
-
-IO.puts handle_open.(File.open("hello.exs"));
-IO.puts handle_open.(File.open("not-found.exs"));
