ccd8bd5130f0806a11f1b568830940b744c19196
Andrea Zucchini <azga84@gmail.com>
2017-03-06 03:38:11 +0100

    square list function

--- ch-7/list.exs
+++ ch-7/list.exs
@@ -1,4 +1,7 @@
 defmodule MyList do
   def len([]), do: 0
   def len([ _head | tail]), do: 1 + length(tail)
+
+  def square([]), do: []
+  def square([ head | tail ]), do: [ head * head | square(tail) ]
 end
