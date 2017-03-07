97d2c60426c6640656c291b7cf9b1546db727c58
Andrea Zucchini <azga84@gmail.com>
2017-03-07 14:25:32 +0100

    caesar cipher

--- ch-7/list.exs
+++ ch-7/list.exs
@@ -40,6 +40,24 @@ defmodule MyList do
     Enum.reduce(list, 0, fn acc, value -> _max(acc, value) end)
   end
 
+  # En criptografía, el cifrado César, también conocido como cifrado por desplazamiento,
+  # código de César o desplazamiento de César, es una de las técnicas de cifrado
+  # más simples y más usadas. Es un tipo de cifrado por sustitución en el que
+  # una letra en el texto original es reemplazada por otra letra que se
+  # encuentra un número fijo de posiciones más adelante en el alfabeto.
+  # Por ejemplo, con un desplazamiento de 3, la A sería sustituida por la D
+  # (situada 3 lugares a la derecha de la A), la B sería reemplazada por la E, etc.
+  # Este método debe su nombre a Julio César, que lo usaba para comunicarse con
+  # sus generales.
+  def caesar([], _), do: []
+  def caesar([ head | tail ], n) do
+    [ cypher(head, n) | caesar(tail, n) ]
+  end
+
+  defp cypher(chr, n) do
+    97 + rem(chr + n - 97, 26)
+  end
+
   defp _max(acc, value) when acc <= value, do: value
   defp _max(acc, value) when acc > value, do: acc
 
