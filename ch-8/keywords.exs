70c5cb1589de302e2e1589f6c4144273a065294a
Andrea Zucchini <azga84@gmail.com>
2017-03-14 22:24:58 +0100

    how to choose betwen maps and keyword list

--- ch-8/keywords.exs
+++ ch-8/keywords.exs
@@ -1,3 +1,15 @@
+# How to Choose Between Maps and Keyword Lists
+# Ask yourself these questions (in this order):
+# • Do I want to pattern-match against the contents 
+# (for example, matching a dictionary that has a key of :name somewhere in it)?
+#     If so, use a map.
+# • Will I want more than one entry with the same key?
+#     If so, you’ll have to use the Keyword module.
+# • Do I need to guarantee the elements are ordered?
+#     If so, again, use the Keyword module.
+# • And, if you’ve reached this point:
+#     Use a map
+
 defmodule Canvas do
     @defaults [fg: "black", bg: "white", font: "Merriweather"]
 
