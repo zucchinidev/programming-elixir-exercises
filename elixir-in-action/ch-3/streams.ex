82d65aa7251e59dee65aa4185fe157fa7650e65a
Andrea Zucchini <azga84@gmail.com>
2017-06-21 20:04:08 +0200

    play with streams

---
+++ elixir-in-action/ch-3/streams.ex
@@ -0,0 +1,15 @@
+defmodule Streams do
+  def lazy_transformation do
+    ["Alice", "Bob", "Frank"]
+      |> Stream.with_index
+      |> Enum.each(fn ({value, index}) -> IO.puts "#{index + 1}. #{value}" end)
+  end
+
+  def square_non_negative_numbers do
+    [9, -1, "foo", 25, 49]
+      |> Stream.filter(&(is_number(&1) and &1 > 0))
+      |> Stream.map(&{&1, :math.sqrt(&1)})
+      |> Stream.with_index
+      |> Enum.each(fn ({{input, result}, index}) -> IO.puts "#{index + 1}. sqrt(#{input}) = #{result}" end)
+  end
+end
