63e950fc701d7c0848b1a8bb0714130409ed9a38
Andrea Zucchini <azga84@gmail.com>
2017-06-21 17:21:18 +0200

    create gen server

---
+++ elixir-in-action/ch-6/key_value_store_get_server.ex
@@ -0,0 +1,28 @@
+require ServerProcess
+defmodule KeyValueStoreGenServer do
+  use GenServer
+
+  def init(_) do
+    {:ok, Map.new}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+  def start do
+    GenServer.start(KeyValueStoreGenServer, nil)
+  end
+
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+end
